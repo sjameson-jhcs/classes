91c6e786d05b0288c5cf54e62befd204224c67a5
sjameson-jhcs <sjameson@jhcschool.org>
2020-10-03 13:21:37 -0600

    Added challenge

--- challenge.py
+++ challenge.py
@@ -1,21 +1,38 @@
-# FIZZ BUZZ
+# For this challenge I am NOT expecting your code to do anything when it runs.
+# Instead your final submission will just be a series of classes that are never instantiated.
+# I want to see that you can think about objects and what parameters are necessary to use them in code.
 
-# Your goal is print all the numbers up to a chosen value.
-# However, any number divisible by 3 will be replaced by the word "Fizz"
-# Any number divisible by the number 5 will be replaced by the word "Buzz"
-# Any number divisible by both will be replaced by the word "FizzBuzz"
 
-# 1. Your code should prompt the user to input a number.
-# 2. Start printing every number up to that number with the replacements mentioned above
-# 3. Keep a running list of all numbers that were replaced by Fizz, Buzz, and FizzBuzz separately
-# 4. After the target number is reached, print out the 3 lists
+# The goal is to create a class for every object that would be necessary to build a game of MONOPOLY
+# If you have not played Monopoly (the board game) feel free to ask me or just google it
 
-# Here is an example output for the target number of 16 (Note: yours will probably be on separate lines. That's ok):
-# 1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16
-# The Fizz numbers were [3, 6, 9, 12]
-# The Buzz numbers were [5,10]
-# The FizzBuzz numbers were [15]
+# I'll start you with one example. Please note that I do not need to you to define the logic in each method
+# but it would be nice if you at least give method names like I have in the example
 
-# Hint: to determine if a number is divisible by another you can use the modulo function
-# Modulo give the remainder after dividing. The symbol for modulo is %. Here is an example:
-# 10 % 3 == 1
+class Player:
+    def __init__(self, _token):
+        self.money = 1500
+        self.token = _token
+        self.properties_owned = {}
+        self.current_location = "GO"
+        self.get_out_of_jail_cards_owned = 0
+
+    def pay_money(self):
+        pass  # Notice I am not defining the logic of what these methods do. You do not need to either.
+        # The keyword "pass" just means it will not do anyting yet
+
+    def get_money(self):
+        pass
+
+    def buy_property(self):
+        pass
+
+    def mortgage_property(self):
+        pass
+
+    def pay_mortgage_on_property(self):
+        pass
+
+    def roll_dice(self):
+        pass
+
